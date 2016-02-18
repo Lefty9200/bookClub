70bfe16ac9a38b6f72da508f315c808aa869bb79
nick_d <ngdaniele@gmail.com>
2016-02-18 10:49:36 -0800

    Added alert if searchfield is empty

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,15 @@
 var app = {};
+
+  app.bookSearch = function() {
+    var search = document.getElementById('searchButton');
+    var searchField = document.getElementById('searchInput');
+
+    search.addEventListner('click', apiGrab);
+
+    function apiGrab() {
+      if (searchField === 'Search by Title or Author' || searchField === '') {
+        document.getElementById('alert').innerHTML = 'Please provide search crite' + 
+        'ria for you book!';
+      }
+  }
+}
