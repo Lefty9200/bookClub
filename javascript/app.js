78cae8c172b7430ab33098b39488ff0f65b21b96
nick_d <ngdaniele@gmail.com>
2016-02-22 09:06:34 -0800

    Got API working, was returning a string as opposed to an object. Now it's returning a JSON Object

--- javascript/app.js
+++ javascript/app.js
@@ -29,6 +29,7 @@ function searchToScript() {
   var input = (searchInput.value.split(' '));
   var script = '';
   var result = '';
+  var req = new XMLHttpRequest();
 
   for (var i = 0; i < input.length; i++) {
     if (i != input.length - 1) {
@@ -40,29 +41,24 @@ function searchToScript() {
   result = 'https://www.googleapis.com/books/v1/volumes?q=' + script;
 
   function handleResponse(response) {
-    console.log(response);
-    for (var key in response) {
-      console.log(result.items[key].volumeInfo.title);
-    }
+    
+    console.log(response.items[1].volumeInfo.title);
   }
 
   function get(url, callback) {
     var request = new XMLHttpRequest();
     request.open('GET', url);
     request.onreadystatechange = function() {
-      if (request.readyState === 4 && request.status === 200) {
-        var type = request.getResponseHeader('Content-Type');
-        if (type.indexOf('xml') !== -1 && request.responseXML) {
-          callback(request.responseXML);
-        } else if (type === 'application/json') {
-          callback(JSON.parse(request.responseText));
-        } else {
-          callback(request.responseText);
-        }
-      }
+      callback(JSON.parse(request.responseText));
     };
     request.send(null);
   }
-  return get(result, handleResponse)
+  return get(result, handleResponse);
 }
+
 searchButton.addEventListener('click', searchToScript);
+
+
+
+
+  
