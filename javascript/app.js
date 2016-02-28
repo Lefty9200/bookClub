9f7df748f8eec446b847e929b936086891e6e55f
nick_d <ngdaniele@gmail.com>
2016-02-27 16:56:18 -0800

    Got rid of code redundancies and updated comments. Need to figure out how to pass this to get rid of further redundancies.

--- javascript/app.js
+++ javascript/app.js
@@ -1,45 +1,72 @@
+//--Custom Library--------------------------------------------------------------
+  // Object to store custom library.
+  var _ = {};
+
+  // QuerySelector. So I don't have to keep writing document.getElementByID. 
+  var $ = function(selector) {
+    // Returns value allowing me to use $(seletor) from now on. 
+    return document.querySelector(selector);
+  }
+
+  // each method so I don't have to keep writing for loops. 
+  _.each = function(list, callback) {
+    // If list is an array loop through use a standard for loop.
+    if (Array.isArray(list)) {
+      for (var i = 0; i < list.length; i++) {
+        // Calls function to be used at a later time passing element, index and
+        // complete list for later use.
+        callback(list[i], i, list);
+      }
+    } else {
+      // If list is not an array it must be an object so use a for in loop.
+      for (var key in list) {
+        // Replaces i with key because using for in.
+        callback(list[key], key, list);
+      }
+    }
+  }
+
+  // Make element function
+  // Make div function
 //------------------------------------------------------------------------------
-  // Global variables
-    // Button for search
-    var searchButton = document.getElementById('searchButton');
-    // Array of search results
-    var response = [];
-    // Array of books on list
+
+
+//------------------------------------------------------------------------------
+  // Global variables.
+    // Button for search.
+    var searchButton = $('#searchButton');
+    // Array of search results.
+    var searchResponse = [];
+    // Array of books on list.
     var listofBooks = [];
-    // Array of completed books
+    // Array of completed books.
     var completedBooks = [];
 //------------------------------------------------------------------------------
 
 
 //------------------------------------------------------------------------------
-  // Make element function
-  // Make div function
-  // Make query selector
-  // Make _.each
-//------------------------------------------------------------------------------
-
-
-//------------------------------------------------------------------------------
-  // Retrieve API results
+  // Retrieve API results.
   function bookApi() {
-    // Variable for search parameters
-    var searchInput = document.getElementById('searchInput');
+    // Variable for search parameters.
+    var searchInput = $('#searchInput');
     
-    // Clear any previous search results
-    response = [];
-    document.getElementById('searchResults').innerHTML = '';
+    // Clear any previous search results.
+    searchResponse = [];
+    $('#searchResults').innerHTML = '';
     
-    // Display search results
+    // Add information needed for app to a usable array named searchResponse.
     function handleResponse() {
-      // Parse JSON Data for use
+      // Parse JSON Data for use.
       var convert = JSON.parse(this.responseText);
 
-      // Call function to handle creation of a results object
-      for (var i = 0; i < convert.items.length; i++) {
-        var item = convert.items[i];
+      // Call function to handle creation of a results object.
+      _.each(convert.items, createResult);
+      function createResult(element) {
+        // Item = element at index.
+        var item = element;
 
-        // Create object out of api results and push to global variable for use
-        response.push({ 
+        // Create object out of api results and push to global variable for use.
+        searchResponse.push({ 
           title: item.volumeInfo.title,
           author: item.volumeInfo.authors[0],
           description: item.volumeInfo.description,
@@ -47,7 +74,8 @@
           thumbnail: item.volumeInfo.imageLinks.thumbnail
         });
       }
-      return searchResults();
+      // Returns searchResult to display actual information to DOM. 
+      return searchResult();
     }
 
 
@@ -57,7 +85,7 @@
       var request = new XMLHttpRequest();
       // Forces handleResponse to fire after load is complete
       request.addEventListener('load', handleResponse);
-      // Opening request using url from result from searchToScript
+      // Opening request using url from result from scriptForSearch
       request.open('GET', url);
       // Sends request
       request.send();
@@ -65,45 +93,49 @@
 
 
     // Creates script for search
-    function searchToScript() {
-      // Converts string to entered into an array
+    function scriptForSearch() {
+      // Converts string entered into an array
       var input = searchInput.value.split(' ');
-      // Script variable holds string with +'s added
-      var script = '';
+      // scriptString holds string with +'s added
+      var scriptString = '';
       // Holds the resulting url
       var result = '';
 
       // Loops through array and adds + were applicable
-      for (var i = 0; i < input.length; i++) {
-        if (i != input.length - 1) {
-          script += input[i] + '+'; 
+      _.each(input, createScript);
+      function createScript(element, index) {
+        // If search parameter is not the last parameter add '+'. 
+        if (index != input.length - 1) {
+          scriptString += input[index] + '+'; 
         } else {
-          script += input[i];
+          // If search paramter is the last one don't add '+'.
+          scriptString += input[index];
         }
       }
-      result = 'https://www.googleapis.com/books/v1/volumes?q='+ script;
-      // fires get function
+      // Add custom string to google api tag.
+      result = 'https://www.googleapis.com/books/v1/volumes?q='+ scriptString;
+      // Fires get function which gets the actual material from api.
       return get(result);
     }
-    // fires searchToScript function
-    return searchToScript();
+    // Fires scriptForSearch function to create custom search based on input.
+    return scriptForSearch();
   }
-  // Add event listener to search button
-  // Changed for now so i don't need to keep populating. Remember to change back!
+  // Add event listener to search button so when clicked bookApi fires.
   searchButton.addEventListener('click', bookApi);
 //------------------------------------------------------------------------------
 
 
 //------------------------------------------------------------------------------
   // Populate search results
-  function searchResults() {
-    for (var i = 0; i < response.length; i++) {
+  function searchResult() {
+    _.each(searchResponse, createDiv);
+    function createDiv(element) {
       // Create new div 
       var newDiv = document.createElement('div');
       newDiv.className = 'searchResult';
       // Create new paragraph for div
       var newParagraph = document.createElement('p');
-      newParagraph.innerHTML = response[i].title;
+      newParagraph.innerHTML = element.title;
       newDiv.appendChild(newParagraph);
 
       // Create add button
@@ -124,16 +156,16 @@
 //------------------------------------------------------------------------------
   // Add book to listofbooks array.
   function addToListofBooks() {
-    for (var i = 0; i < response.length; i++) {
-      if (this.parentNode.firstChild.innerHTML === response[i].title) {
-        listofBooks.push(response[i]);
+    for (var i = 0; i < searchResponse.length; i++) {
+      if (this.parentNode.firstChild.innerHTML === searchResponse[i].title) {
+        listofBooks.push(searchResponse[i]);
 
         // Create new div 
         var newDiv = document.createElement('div');
         newDiv.className = 'book';
         // Create new paragraph for div
         var newParagraph = document.createElement('p');
-        newParagraph.innerHTML = response[i].title;
+        newParagraph.innerHTML = searchResponse[i].title;
         newDiv.appendChild(newParagraph);
 
         // Create star button
@@ -151,7 +183,7 @@
         // Add each book on top of the last
         var currentDiv = document.getElementById('div1');
         document.getElementById('bookList').insertBefore(newDiv, currentDiv);
-        starButton.onclick = setAsCurrent;
+        // starButton.onclick = setAsCurrent;
         removeButton.onclick = removeBook;
       }
     }
@@ -184,11 +216,9 @@
 
 
 /*----Tweaqs--------------------------------------------------------------------
-  - Consolidate code written in app.js and app1.js.
   - Need to add alerts back into search. It needs to alert when search input is 
     empty on 'click' and on 'input'. So when user it filling alert goes away.
   - Need to make the return key work for search.
   - Make it so if book is already on reading list you can delete it.
   - Make it so api doesn't grab sample books or duplicates.
-  - Refactor code to get rid of redundancies.
 ------------------------------------------------------------------------------*/
