af85bef40a4418c1817b82053fc998804b1aa34f
nick_d <ngdaniele@gmail.com>
2017-01-11 20:20:20 -0800

    Webpack configured and working

---
+++ client/main.js
@@ -0,0 +1,2 @@
+import * as math from "./test";
+console.log("2π = " + math.sum(math.pi, math.pi));
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,55 @@
-module.exports = {
-  entry: "./client/main.js",
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+  context: __dirname,
+  entry: './client/main.js',
   output: {
-    path: __dirname,
-    filename: "bundle.js"
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/build')
+  },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  },
+  devServer: {
+      contentBase: "./client",
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.js$/, 
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/, 
+        loader: 'style-loader!css-loader'
+      },
+      { 
+        test: /\.png$/, 
+        loader: "url-loader?limit=100000" 
+      },
+      { 
+        test: /\.jpg$/, 
+        loader: "file-loader" 
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }
     ]
   }
 };
+
+module.exports = config;
